e32227ad6e7fcc0e5000585054eea1db70ee6178
Abdi-01 <abdialghi@gmail.com>
2022-01-12 21:52:12 +0700

    CN:016 : update cart

--- pages/api/helper.js
+++ pages/api/helper.js
@@ -1,4 +1,4 @@
-// export const API_URL = `https://jserver-ecommerce19.herokuapp.com`
-export const API_URL = `http://localhost:2010`
+export const API_URL = `https://jserver-ecommerce19.herokuapp.com`
+// export const API_URL = `http://localhost:2010`
 
 export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
